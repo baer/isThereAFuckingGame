1ae6e8a9e94b9304387713f158e2e29eedcbf7e9
Eric Baer <me@ericbaer.com>
2015-05-11 15:29:43 -0700

    Remove unused css buld task

--- gulp-tasks/browser/build-css.js
+++ gulp-tasks/browser/build-css.js
@@ -7,16 +7,9 @@ var prefix = require('gulp-autoprefixer');
 
 var config = require('../../project.config');
 
-gulp.task('build:css', 'Build CSS & LESS --> CSS.', [
-  'build:css:css',
+gulp.task('build:css', 'Build LESS --> CSS.', [
   'build:css:less'
 ]);
-
-gulp.task('build:css:css', 'Build and add vendor prefixes for plain CSS.', function () {
-  return gulp.src(path.join(config.srcFullPath, config.styles, '*.css'))
-    .pipe(prefix('last 1 version', '> 1%', 'ie 8'))
-    .pipe(gulp.dest(path.join(config.destFullPath, 'styles')));
-});
 
 gulp.task('build:css:less', 'Build and add vendor prefixes to LESS styles.', function () {
   return gulp.src(path.join(config.srcFullPath, config.styles, '*.less'))
