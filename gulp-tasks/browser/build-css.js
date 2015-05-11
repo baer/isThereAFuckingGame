47558bed8a58aeaf13ac87f51e93d8a9867f4904
Eric Baer <me@ericbaer.com>
2015-05-11 15:33:43 -0700

    Add CSS minification

--- gulp-tasks/browser/build-css.js
+++ gulp-tasks/browser/build-css.js
@@ -2,6 +2,7 @@
 
 var path = require('path');
 var gulp = require('gulp');
+var minifyCSS = require('gulp-minify-css');
 var less = require('gulp-less');
 var prefix = require('gulp-autoprefixer');
 
@@ -15,5 +16,6 @@ gulp.task('build:css:less', 'Build and add vendor prefixes to LESS styles.', fun
   return gulp.src(path.join(config.srcFullPath, config.styles, '*.less'))
     .pipe(less())
     .pipe(prefix('last 1 version', '> 1%', 'ie 8'))
+    .pipe(minifyCSS())
     .pipe(gulp.dest(path.join(config.destFullPath, 'styles')));
 });
