e779c64a4cf20341ca491f49d0f91e466a95a9d5
Eric Baer <me@ericbaer.com>
2015-05-11 14:13:43 -0700

    Remove dead project config

--- project.config.js
+++ project.config.js
@@ -9,7 +9,4 @@ config.srcFullPath = path.join(config.root, config.src);
 config.destFullPath = path.join(config.root, config.dest);
 
 config.js = 'js';
-config.assets = 'assets';
 config.styles = 'styles';
-config.karmaConfig = 'karma.conf.js';
-config.testRunner = path.join(config.src, '../test-runner.js');
