61b161cfdb07cafd442dcf06c663735a38e8d2e9
Eric Baer <me@ericbaer.com>
2015-05-11 14:50:53 -0700

    Remove extranious comments from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,6 @@ var config = require('./project.config');
 module.exports = {
   context: config.srcFullPath,
 
-  /**
-   * Based on the configuration below, a `main.bundle.js` file will be generated
-   * from the file specified in `entry.main` along with its dependencies. You can
-   * create additional entry points and Webpack will intelligently bundle them
-   * for you.
-   */
-
   entry: {
     main: './js/main.js'
   },
@@ -20,21 +13,7 @@ module.exports = {
     filename: '[name].bundle.js',
     chunkFilename: '[id].bundle.js'
   },
-
-  /**
-   * The following loaders define alternate behavior for `require` expressions in
-   * your JS modules.
-   *
-   * Any CSS files will go through the style-loader (meaning that
-   * they'll be loaded into the document when the JS file is required).  Any
-   * Stylus files will be processed by Stylus to generate CSS, run through auto-
-   * prefixer to add CSS vendor prefixes, and then treated as CSS files inside
-   * your application.
-   *
-   * Files ending in `.tmpl` will be required into your application as plain text.
-   * Modify this entry or create a new entry if you prefer to use a different
-   * extension for your templates.
-   */
+  devtool: "sourcemap",
 
   module: {
     loaders: [
@@ -42,6 +21,14 @@ module.exports = {
       { test: /\.json$/, loader: 'json-loader' }
     ]
   },
+
+  plugins: [
+    // new webpack.DefinePlugin({
+    //   'ENVIRONMENT': JSON.stringify('PROD')
+    // }),
+    // new webpack.optimize.DedupePlugin(),
+    // new webpack.optimize.UglifyJsPlugin()
+  ],
 
   /**
    * This defines the 'root' of your project.  If you `require('some-package')`
@@ -51,18 +38,5 @@ module.exports = {
 
   resolve: {
     root: config.root
-  },
-
-  /**
-   * For production deployments, we want minified and optimized JS.  These
-   * settings are overridden in webpack.dev.config.js.
-   */
-
-  plugins: [
-    // new webpack.DefinePlugin({
-    //   'ENVIRONMENT': JSON.stringify('PROD')
-    // }),
-    // new webpack.optimize.DedupePlugin(),
-    // new webpack.optimize.UglifyJsPlugin()
-  ]
+  }
 };
