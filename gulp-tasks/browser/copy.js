26421e9be6fa89df13ad484bc349dd418d180ee6
Eric Baer <me@ericbaer.com>
2015-05-11 15:36:46 -0700

    Remove copying of data file

--- gulp-tasks/browser/copy.js
+++ gulp-tasks/browser/copy.js
@@ -6,31 +6,24 @@ var config = require('../../project.config');
 
 gulp.task('copy', 'Copy all raw files into dist', [
   'copy:root',
-  'copy:data',
   'copy:fonts',
   'copy:images'
 ]);
 
-gulp.task('copy:root', function () {
+gulp.task('copy:root', 'Copy raw files at the root of `src/client` into dist', function () {
   return gulp.src([
     path.join(config.srcFullPath, '.htaccess'),
     path.join(config.srcFullPath, '*')
   ]).pipe(gulp.dest(config.destFullPath));
 });
 
-gulp.task('copy:data', function () {
-  return gulp.src([
-    path.join(config.srcFullPath, '../data/**/*')
-  ]).pipe(gulp.dest(path.join(config.destFullPath, 'data')));
-});
-
-gulp.task('copy:fonts', function () {
+gulp.task('copy:fonts', 'Copy fonts into dist', function () {
   return gulp.src([
     './node_modules/font-awesome/fonts/**/*'
   ]).pipe(gulp.dest(path.join(config.destFullPath, 'fonts')));
 });
 
-gulp.task('copy:images', function () {
+gulp.task('copy:images', 'Copy images into dist', function () {
   return gulp.src([
     './node_modules/bootstrap/img/**/*'
   ]).pipe(gulp.dest(path.join(config.destFullPath, 'img')));
