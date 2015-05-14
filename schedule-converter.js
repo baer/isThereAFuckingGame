a3d8b2c684157e95f9508d0d40d8b2b88fc96c1c
Eric Baer <me@ericbaer.com>
2015-05-14 10:31:03 -0700

    Update the csv parser to auto detect home team

--- schedule-converter.js
+++ schedule-converter.js
@@ -6,19 +6,18 @@ var fs = require('fs');
 var _ = require('lodash');
 var moment = require('moment-timezone');
 
-var teamName = 'Rockies';
 var srcPath = path.resolve('./src/data/schedule.csv')
 var destPath = path.resolve('./src/data') + '/schedule.json';
 
-var transform = function(csvData) {
+var transform = function(csvData, homeTeam) {
   return _.map(csvData.slice(1), function(row) {
     var gameData = _.zipObject(csvData[0], row);
 
-    var date = moment(gameData.START_DATE + ' ' + gameData.START_TIME_ET, 'MM/DD/YYYY hh:mm a')
+    var date = moment(gameData.START_DATE + ' ' + gameData.START_TIME_ET, 'MM/DD/YY hh:mm a')
       .tz('America/New_York');
 
-    var opponent = _.find(gameData.SUBJECT.split(' '), function(word) {
-      return word !== teamName && word !== 'at';
+    var opponent = _.find(gameData.SUBJECT.split(' at '), function(word) {
+      return word !== homeTeam;
     });
 
     return {
@@ -27,6 +26,33 @@ var transform = function(csvData) {
       opponent: opponent,
     };
   });
+};
+
+var getHomeTeam = function(csvData) {
+  return _.chain(csvData.slice(1))
+    // Generate an Array of Arrays of all of the teams that play each other
+    // D-backs at Rockies -> ['D-backs', 'Rockies']
+    .map(function(row) {
+      var gameData = _.zipObject(csvData[0], row);
+      return gameData.SUBJECT.split(' at ');
+    })
+    .flatten()
+    // Reduce to an object containing unique team names and a count of occurances
+    .reduce(function(memo, team) {
+      if (!memo[team]) { memo[team] = 0; }
+      memo[team]++;
+      return memo;
+    }, {})
+    // Return the team that occurs the most
+    .reduce(function(memo, gamesPlayed, team) {
+      if (gamesPlayed > memo.gamesPlayed) {
+        memo.gamesPlayed = gamesPlayed;
+        memo.team = team;
+      }
+      return memo;
+    }, { team: '', gamesPlayed: 0})
+    .value()
+    .team;
 }
 
 fs.readFile(srcPath, function (err, data) {
@@ -35,7 +61,10 @@ fs.readFile(srcPath, function (err, data) {
   csv.parse(data, function(err, data){
     if (err) throw err;
 
-    fs.writeFile(destPath, JSON.stringify(transform(data)), function (err) {
+    var homeTeam = getHomeTeam(data);
+    var jsonData = transform(data, homeTeam);
+
+    fs.writeFile(destPath, JSON.stringify(jsonData), function (err) {
       if (err) throw err;
       console.log('Finished!');
     });
