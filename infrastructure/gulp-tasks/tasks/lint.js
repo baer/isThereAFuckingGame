8a0abd6b956aa6ea50097d4390e511d1ad485e67
Eric Baer <me@ericbaer.com>
2016-04-10 11:27:55 -0700

    Remove unneeded dep by simplifying the lint task

--- infrastructure/gulp-tasks/tasks/lint.js
+++ infrastructure/gulp-tasks/tasks/lint.js
@@ -1,35 +1,23 @@
-'use strict';
+"use strict";
 
-var path = require('path');
-var gulp = require('gulp');
-var _ = require('lodash');
-var gutil = require('gulp-util');
-var map = require('map-stream');
-var eslint = require('gulp-eslint');
-var config = require('../../../project.config');
+const eslint = require("gulp-eslint");
+const gulp = require("gulp");
+const path = require("path");
 
-gulp.task('lint', 'Lint application', function (cb) {
-  var success = true;
+const config = require('../../../project.config');
+const handleErrors = require("../util/handle-errors");
 
-  gulp.src([
+gulp.task("lint:all", false, () => {
+  return gulp.src([
     path.join(config.srcFullPath, '**/*.js'),
     path.join(config.root, '*.js'),
     path.join(config.root, 'gulp-tasks/**/*.js')
   ])
     .pipe(eslint({configFile: "./infrastructure/eslint/.eslintrc"}))
-    .pipe(map(function (file, output) {
-      success = success && _.every(file.eslint && file.eslint.messages, function (message) {
-        return message.severity !== 2;
-      });
-      return output(null, file);
-    }))
     .pipe(eslint.format())
-    .on('end', function () {
-      if (success) {
-        gutil.log('[lint]', 'SUCCESS!');
-        cb();
-      } else {
-        cb(new gutil.PluginError('lint', '*** FAILED ESLINT ***'));
-      }
-    });
+    .pipe(eslint.failAfterError());
 });
+
+gulp.task("lint", "run lint tasks", [
+  "lint:all"
+]);
