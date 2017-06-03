0fee341a569554f5ceaee64805d2efd6f0571499
Eric Baer <me@ericbaer.com>
2017-06-03 00:22:34 -0400

    Use define plugin with process.env rather than hard code

--- infrastructure/webpack/webpack.config.js
+++ infrastructure/webpack/webpack.config.js
@@ -39,8 +39,7 @@ module.exports = {
     new DefinePlugin({
       // Signal production, so that webpack removes non-production code that
       // is in condtionals like: `if (process.env.NODE_ENV === "production")`
-      // "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-      "process.env.NODE_ENV": JSON.stringify("production")
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
     }),
     new BabiliPlugin({}, {
       sourceMap: true
