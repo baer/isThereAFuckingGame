2e17c92059e8a71efaf61a683ff6c48aa9429cc1
Eric Baer <me@ericbaer.com>
2022-09-03 01:30:44 -0600

    Add strict mode and disable runtime image opt in Next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  reactStrictMode: true,
+  // Since the site is exposted statically, the fancy Next.js image opts need
+  // to be turned off.
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
   env: {
     homeTeam: "Rockies",
     homeStadium: "Coors Field - Denver",
